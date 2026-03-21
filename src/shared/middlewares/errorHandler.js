d3a342f71a303adde4078b5bad4373b56b800e38
Priyanshu Verma <work.priyanshuverma.1310@gmail.com>
2026-03-21 20:04:58 +0530

    feat: map specific mongoose and jwt validation exceptions within error handler

--- src/shared/middlewares/errorHandler.js
+++ src/shared/middlewares/errorHandler.js
@@ -7,6 +7,7 @@ const errorHandler = (err, req, res, next) => {
     let message = err.message || "Internal server error";
     let errors = err.errors || null
 
+
     logger.error('Error occurred:', {
         message: err.message,
         statusCode,
@@ -14,6 +15,21 @@ const errorHandler = (err, req, res, next) => {
         path: req.path,
         method: req.method,
     });
+
+    if (err.name === "ValidationError") {
+        statusCode = 400;
+        message = "Validation Error";
+        errors = Object.values(err.errors).map((e) => e.message)
+    } else if (err.name === 'MongoServerError' && err.code === 11000) {
+        statusCode = 409;
+        message = 'Duplicate key error';
+    } else if (err.name === 'JsonWebTokenError') {
+        statusCode = 401;
+        message = 'Invalid token';
+    } else if (err.name === 'TokenExpiredError') {
+        statusCode = 401;
+        message = 'Token expired';
+    };
 
     res.status(statusCode).json(ResponseFormatter.error(message, statusCode, errors))
 }
