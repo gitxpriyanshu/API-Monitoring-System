0811c32a47f80e1fcba43093fc00adff1b8c35f7
Priyanshu Verma <work.priyanshuverma.1310@gmail.com>
2026-03-22 04:25:50 +0530

    feat(auth): wire auth router with dependency injection and public routes

--- src/services/auth/routes/authRouter.js
+++ src/services/auth/routes/authRouter.js
@@ -1,9 +1,26 @@
-import express from 'express';
+import express from "express";
+import dependencies from "../Dependencies/dependencies.js"
+import authorize from "../../../shared/middlewares/authorize.js"
+import authenticate from "../../../shared/middlewares/authenticate.js"
+import validate from "../../../shared/middlewares/validate.js";
+import requestLogger from "../../../shared/middlewares/requestLogger.js";
+import { onboardSuperAdminSchema, loginSchema } from "../validation/authSchema.js";
+import { APPLICATION_ROLES } from "../../../shared/constants/roles.js";
 
 const router = express.Router();
+const { controllers } = dependencies;
+const authController = controllers.authController
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Auth routes working' });
-});
+router.post("/onboard-super-admin",
+    requestLogger,
+    validate(onboardSuperAdminSchema),
+    (req, res, next) => authController.onboardSuperAdmin(req, res, next)
+)
 
-export default router;
+router.post("/login",
+    requestLogger,
+    validate(loginSchema),
+    (req, res, next) => authController.login(req, res, next)
+);
+
+export default router
